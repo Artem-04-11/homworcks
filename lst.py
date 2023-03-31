8b1c23c2e2959dbb0bb751b0ede411b9c8427a81
Artem <artem.ayvazyan88@gmail.com>
2023-03-31 21:05:33 +0400

    added homework1

--- lst.py
+++ lst.py
@@ -1,6 +1,7 @@
-def my_list(ls):
-    ls1 = [el * (-1) if 3 <= el <= 8 else el for el in ls]
-    return ls1
-     
-ls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(my_list(ls))
+def printNumber(n, k = '\n'):
+  if n > 0:
+    printNumber(n - 1, k = ',')
+    print(n, end = k)
+
+n = int(input())
+printNumber(n)
